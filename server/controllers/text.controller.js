118322bbbf343e773a49e1beafecb86c968f255f
Akin <akinkunmishonibare@gmail.com>
2018-10-12 13:21:18 -0400

    quick fix

--- server/controllers/text.controller.js
+++
@@ -1,8 +0,0 @@
-exports.determineQuery = function(req, res, next) {
-  var response = req.body;
-  res.json(response);
-};
-
-exports.showSomething = function(req, res, next) {
-  res.json({ name: "john doe" });
-};
--- server/routes/text.routes.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var text_controller = require("../controllers/text.controller");
-
-router.post("/", text_controller.determineQuery);
-router.get("/", text_controller.showSomething);
-
-module.exports = router;
