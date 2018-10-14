b39966753dc8ae2dff563d24ae9ca6ede67590ff
Akin <akinkunmishonibare@gmail.com>
2018-10-13 21:46:15 -0400

    initiated a simple twilio api call

--- server/controllers/text.controller.js
+++ server/controllers/text.controller.js
@@ -1,7 +1,14 @@
+var MessagingResponse = require("twilio").twiml.MessagingResponse;
+
 exports.determineQuery = function(req, res, next) {
   var response = req.body;
-  res.json(response);
+  console.log(response);
+  const twiml = new MessagingResponse();
+  twiml.message("thanks for using textnet");
+
+  res.writeHead(200, { "Content-Type": "text/xml" });
+  res.end(twiml.toString());
 };
- exports.showSomething = function(req, res, next) {
+exports.showSomething = function(req, res, next) {
   res.json({ name: "john doe" });
 };
