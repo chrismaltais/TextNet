56509f2d1b589bf6d9be5a03426e09abf8827afc
Akin <akinkunmishonibare@gmail.com>
2018-10-14 23:36:07 -0400

    initiated twilio api and google translation api

--- server/app.js
+++ server/app.js
@@ -7,15 +7,16 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var cors = require("cors");
- //routes
+//routes
 var indexRouter = require("./routes/index.routes");
 var textRouter = require("./routes/text.routes");
- var app = express();
- // view engine setup
+
+var app = express();
+// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
- app.use(cors());
- app.use(function(req, res, next) {
+app.use(cors());
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
@@ -23,7 +24,7 @@ app.set("view engine", "pug");
   );
   next();
 });
- // Set up a whitelist and check against it:
+// Set up a whitelist and check against it:
 // var whitelist = ['http://example1.com', 'http://example2.com']
 // var corsOptions = {
 //   origin: function (origin, callback) {
@@ -39,32 +40,33 @@ app.set("view engine", "pug");
 // app.use(cors(corsOptions));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
- app.use(logger("dev"));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
- //Set up mongoose connection
+//Set up mongoose connection
 const { MONGO_USER, MONGO_PASS, MONGO_URL } = process.env;
 CONNECTION_URI = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_URL}`;
- mongoose
+mongoose
   .connect(CONNECTION_URI)
   .then(() => console.log("Connected to Database"))
   .catch(e => console.error(e));
- app.use("/", indexRouter);
+app.use("/", indexRouter);
 app.use("/text", textRouter);
- // catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
- // error handler
+// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-   // render the error page
+  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
- module.exports = app;
+
+module.exports = app;
--- server/controllers/text.controller.js
+++ server/controllers/text.controller.js
@@ -1,14 +1,76 @@
 var MessagingResponse = require("twilio").twiml.MessagingResponse;
+const { Translate } = require("@google-cloud/translate");
+var async = require("async");
+const fs = require("fs");
 
-exports.determineQuery = function(req, res, next) {
-  var response = req.body;
-  console.log(response);
-  const twiml = new MessagingResponse();
-  twiml.message("thanks for using textnet");
+var finalTranslation = "";
+exports.determineQuery = async function(req, res, next) {
+  var query = req.body.Body;
 
-  res.writeHead(200, { "Content-Type": "text/xml" });
-  res.end(twiml.toString());
+  var queryArr = query.split(" ");
+  task = queryArr[0];
+
+  if (task == "translate") {
+    var stringToTranslate = query.substring(
+      query.lastIndexOf("translate ") + 10,
+      query.lastIndexOf("to ")
+    );
+
+    // var stringToTranslate = query.substring(
+    //   query.lastIndexOf("translate ") + 10,
+    //   query.lastIndexOf("from ")
+    // );
+
+    // var languageFrom = query.substring(
+    //   query.lastIndexOf("from ") + 5,
+    //   query.lastIndexOf("to ")
+    // );
+
+    var languageTo = query.substring(
+      query.lastIndexOf("to ") + 3,
+      query.lastIndexOf("")
+    );
+
+    translation = "";
+    translate(stringToTranslate, languageTo).then(val => {
+      const twiml = new MessagingResponse();
+      twiml.message(val);
+      res.writeHead(200, { "Content-Type": "text/xml" });
+      res.end(twiml.toString());
+    });
+  }
 };
 exports.showSomething = function(req, res, next) {
   res.json({ name: "john doe" });
 };
+
+async function translate(stringToTranslate, languageTo) {
+  // Your Google Cloud Platform project ID
+  translateResult = "";
+
+  // Instantiates a client
+  const translate = new Translate({
+    projectId: process.env.GOOGLE_PROJECT_ID
+  });
+  let rawdata = fs.readFileSync("./languageCode.json");
+  let languages = JSON.parse(rawdata);
+  for (var i = 0; i < languages.length; i++) {
+    if (languageTo.toLowerCase() === languages[i]["name"].toLowerCase()) {
+      var langCode = languages[i]["code"];
+    }
+  }
+
+  return await new Promise((resolve, reject) => {
+    translate
+      .translate(stringToTranslate, langCode)
+      .then(results => {
+        const translation = results[0];
+        console.log(`Text: ${stringToTranslate}`);
+        console.log(`Translation: ${translation}`);
+        resolve(translation);
+      })
+      .catch(err => {
+        console.error("ERROR:", err);
+      });
+  });
+}
