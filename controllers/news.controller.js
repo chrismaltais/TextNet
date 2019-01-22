cc7740d4c516e178d75f0749a0642a5d2d130c36
Chris Maltais <chris.maltais@hotmail.ca>
2019-01-21 23:18:06 -0500

    ✨ Add news functionality

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,23 +1,14 @@
-async function getResponse(message) {
-  axios
-    .get(
-      `https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=e7817483252a45378feffdb9cda39573`
-    )
-    .then(function(response) {
-      for (var i = 0; i < 5; i++) {
-        res.push(
-          `${response.data.articles[i].title} \n ${
-            response.data.articles[i].description
-          } \n\n`
-        );
-      }
-    })
-    .then(() => {
-      return res.join("");
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+const axios = require('axios')
+
+async function getResponse() {
+  let res = [];
+  let response = await axios.get(`https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=e7817483252a45378feffdb9cda39573`);
+
+  for (var i = 0; i < 5; i++) {
+    res.push(`${response.data.articles[i].title} \n ${response.data.articles[i].description} \n\n`);
+  }
+
+  return res.join('');
 }
 
 module.exports = {
