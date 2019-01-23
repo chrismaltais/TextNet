16d2bafe795c9a4eb431852a3cbf7dbd654d650c
Cory <ccrowley96@gmail.com>
2019-01-23 16:22:39 -0500

    removed name Wolfram Alpha from responses

--- server/controllers/wolfram.controller.js
+++ server/controllers/wolfram.controller.js
@@ -1,12 +1,12 @@
 const axios = require("axios");
 
 async function getResponse(message) {
-  let res = [];
   let response = await axios.get(
     `http://api.wolframalpha.com/v1/result?appid=WLAQQ9-YP923PK67P&i=${message}`
   );
 
-  return response.data;
+  let result = response.data.replace("Wolfram Alpha", "TextNet");;
+  return result;
 }
 
 module.exports = {
