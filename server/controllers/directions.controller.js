6a6e8c18df0dd87402ca96d08a5213a7702d662c
Cory <ccrowley96@gmail.com>
2019-01-23 17:16:01 -0500

    made directions more robust (not throw errors)

--- server/controllers/directions.controller.js
+++ server/controllers/directions.controller.js
@@ -5,8 +5,19 @@ let directionList = [];
 async function getResponse(message) {
     let res = '';
 
+    // Check for validity of formatting
+    if(message.indexOf(" from ") === -1 || message.indexOf(" to ") === -1)
+    {
+        return 'Invalid direction formatting.  You\'re likely missing the "from" or "to" keywords! Texting "textnet" will return our formatting guidelines :)';
+    }
+
+    console.log("passed invalid test");
+
     let origin_addr = encodeURIComponent(message.substring(message.indexOf("from") + 5, message.lastIndexOf(" to ")).trim());
     let dest_addr = encodeURIComponent(message.substring(message.lastIndexOf(" to ") + 4, message.length).trim());
+
+    console.log(message.substring(message.indexOf("from") + 5, message.lastIndexOf(" to ")).trim());
+    console.log(message.substring(message.lastIndexOf(" to ") + 4, message.length).trim());
 
     let tranportation_mode = 'driving';
 
@@ -27,13 +38,19 @@ async function getResponse(message) {
     return await axios.get(`https://maps.googleapis.com/maps/api/directions/json?origin=${origin_addr}&destination=${dest_addr}&mode=${tranportation_mode}&key=${process.env.MAPS_API_KEY}`)
     .then(function (response) {
         //Parse Directions
+        if(response.data.status === 'NOT_FOUND') {
+            return "NOT_FOUND";
+        }
         response.data.routes[0].legs[0].steps.forEach(element => {
             let curr_step = element.html_instructions +" ("+ element.distance.text+")";
             let regex = /(&nbsp;|<([^>]+)>)/ig;
             directionList.push(striptags(curr_step,["div"]).replace(regex, " "));
             res = directionList.join('\n\n');
          });
-    }).then(() =>{
+    }).then((err) =>{
+        if(err){
+            return "Directions not found :(";
+        }
         let trans_mode = tranportation_mode + " directions ";
         if(res.length > 1500){
             return trans_mode + "limited to 1500 chars...\n" + res.substr(0,1500) + "\n.....";
