5aec0769c17191534e299bda90e414cfc91aac24
Cory <ccrowley96@gmail.com>
2019-02-07 16:18:44 -0500

    added better error msg to news controller

--- server/controllers/news.controller.js
+++ server/controllers/news.controller.js
@@ -2,7 +2,15 @@ const axios = require('axios')
 
 async function getResponse() {
   let res = [];
-  let response = await axios.get(`https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=e7817483252a45378feffdb9cda39573`);
+  let err_msg = "Sorry cannot find news right now.  It's not you, it's us </3";
+  let response = await axios.get(`https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=${process.env.NEWS_KEY}`).catch(function (error) {
+    console.log(error);
+  });
+
+  console.log(response);
+  if(response.status != 200){
+    return err_msg;
+  }
 
   for (var i = 0; i < 5; i++) {
     res.push(`${response.data.articles[i].title} \n ${response.data.articles[i].description} \n\n`);
