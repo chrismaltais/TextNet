ddba18336a0e1274f63c3107efec6f65c945c9bd
Cory <ccrowley96@gmail.com>
2019-02-07 16:40:34 -0500

    fixed wolfram number input

--- server/controllers/wolfram.controller.js
+++ server/controllers/wolfram.controller.js
@@ -4,14 +4,14 @@ async function getResponse(message) {
   let response;
   try {
     response = await axios.get(
-      `http://api.wolframalpha.com/v1/result?appid=WLAQQ9-YP923PK67P&i=${message}`
+      `http://api.wolframalpha.com/v1/result?appid=WLAQQ9-YP923PK67P&i=${message.toString()}`
     );
   } catch (e) {
     errorString = `Invalid query: Unfortunately TextNet isn't smart enough to figure that one out. Ask us another question! :)`
     return  errorString
   }
 
-  let result = response.data.replace("Wolfram Alpha", "TextNet");
+  let result = response.data.toString().replace("Wolfram Alpha", "TextNet");
   return result;
 }
 
