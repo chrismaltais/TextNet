a7e1ba978e32f7eb7f02dd29348fb5abdac4ae3d
Chris Maltais <chris.maltais@hotmail.ca>
2019-03-20 13:15:22 -0400

    🚧 Break Directions controller to multiple funcs

--- server/controllers/directions.controller.js
+++ server/controllers/directions.controller.js
@@ -2,63 +2,46 @@ const axios = require('axios');
 const striptags = require('striptags');
 
 async function getResponse(message) {
-    let resultString = '';
     let invalidFormattingMessage = 'Invalid direction formatting.  You\'re likely missing the "from" or "to" keywords! Texting "textnet" will return our formatting guidelines :)';
-    let directionList = [];
+
+    // Trim extra spaces before and after message
+    message = message.trim(); 
+
     // Check for validity of formatting
-    if (message.includes(" from ") == false || message.includes(" to ") == false) {
+    messageIsValidFormat = isValidFormat(message);
+    if (!messageIsValidFormat) {
         return invalidFormattingMessage;
     }
 
-    message = message.trim(); // trim extra spaces
+    // Get Origin Address
+    let rawOriginAddress = getRawOriginAddress(message);
+    
+    // Get URI Encoded Origin Address
+    let originAddress = encodeURIComponent(rawOriginAdress);
 
-    let originAddrRaw = message.substring(message.indexOf("from") + 5, message.lastIndexOf(" to ")).trim();
-    let originAddress = encodeURIComponent(originAddrRaw);
+    // Get Destination Address
+    let rawDestinationAddress = getRawDestinationAddress(message);
 
-    let destAddressRaw = message.substring(message.lastIndexOf(" to ") + 4, message.length).trim();
-    let destAddress = encodeURIComponent(destAddressRaw);
+    // Get URI Encoded Destination Address
+    let destAddress = encodeURIComponent(rawDestinationAddress);
 
     // Check that origin & destination have contents
     if (!originAddress || !destAddress) {
         return invalidFormattingMessage;
     }
 
-    console.log(`Directions - Origin: ${originAddrRaw}, Destination: ${destAddressRaw}`);
+    console.log(`Directions - Origin: ${rawOriginAddress}, Destination: ${rawDestinationAddress}`);
 
-    let transportationMode = 'driving';
-
-    let userSelectedTransportMode = message.split(" ")[1];
-
-    if (userSelectedTransportMode == ("walk") || userSelectedTransportMode == ("walking")) {
-        transportationMode = 'walking';
-    } else if (userSelectedTransportMode == ("bike") || userSelectedTransportMode == ("biking")) {
-        transportationMode = 'bicycling';
-    } else if (userSelectedTransportMode == ("transit") || userSelectedTransportMode == ("bus")) {
-        transportationMode = 'transit';
-    } else {
-        transportationMode = 'driving';
-    }
+    // Get Transportation Mode
+    let transportationMode = getTransportationMode(message);
 
     let response = await axios.get(`https://maps.googleapis.com/maps/api/directions/json?origin=${originAddress}&destination=${destAddress}&mode=${transportationMode}&key=${process.env.MAPS_API_KEY}`)
     //Parse Directions
     if (response.data.status === 'OK') {
 
-        // Multiple routes, choose the best option (0)
-        // For each instruction do...
-        response.data.routes[0].legs[0].steps.forEach(element => {
-            // Grabs HTML instructions supplied by Google and appends the distance in brackets
-            let currentStepHTML = element.html_instructions + " (" + element.distance.text + ")";
-            // Strips HTML tags from string (found online)
-            // Used to strip <div> tags that will allow for better formatting
-            let regex = /(&nbsp;|<([^>]+)>)/ig;
-
-            // Leave <div> tags because it surrounds the destination step
-            // Replace with " " for final formatting to look great on SMS
-            directionList.push(striptags(currentStepHTML, ["div"]).replace(regex, " "));
-
-            resultString = directionList.join('\n\n');
-        });
-
+        // Parse HTML Directions from Google API
+        let resultString = parseHTMLDirections(response);
+        
         // Confirm transportation mode and destination at beginning of text
         let textPreface = `${transportationMode.charAt(0).toUpperCase() + transportationMode.substring(1)}` + " directions to " + `${destAddressRaw}`;
 
@@ -73,6 +56,56 @@ async function getResponse(message) {
     } else {
         return `Directions not found :( \n Error: ${response.data.status}`;
     }
+}
+
+async function isValidFormat(message) {
+    if (message.includes(" from ") == false || message.includes(" to ") == false) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+async function getRawOriginAddress(message) {
+    return message.substring(message.indexOf("from") + 5, message.lastIndexOf(" to ")).trim();
+}
+
+async function getRawDestinationAddress(message) {
+    return message.substring(message.lastIndexOf(" to ") + 4, message.length).trim();
+}
+
+async function getTransportationMode(message) {
+    let userSelectedTransportMode = message.split(" ")[1];
+
+    if (userSelectedTransportMode == ("walk") || userSelectedTransportMode == ("walking")) {
+        return 'walking';
+    } else if (userSelectedTransportMode == ("bike") || userSelectedTransportMode == ("biking")) {
+        return 'bicycling';
+    } else if (userSelectedTransportMode == ("transit") || userSelectedTransportMode == ("bus")) {
+        return'transit';
+    } else {
+        return 'driving';
+    }
+}
+
+async function parseHTMLDirections(directionsAPIResponseJSON) {
+    let directionResultsString = '';
+    let directionList = [];
+    // Multiple routes, choose the best option (0)
+    // For each instruction do...
+    directionsAPIResponseJSON.data.routes[0].legs[0].steps.forEach(element => {
+        // Grabs HTML instructions supplied by Google and appends the distance in brackets
+        let currentStepHTML = element.html_instructions + " (" + element.distance.text + ")";
+        // Strips HTML tags from string (found online)
+        // Used to strip <div> tags that will allow for better formatting
+        let regex = /(&nbsp;|<([^>]+)>)/ig;
+
+        // Leave <div> tags because it surrounds the destination step
+        // Replace with " " for final formatting to look great on SMS
+        directionList.push(striptags(currentStepHTML, ["div"]).replace(regex, " "));
+
+        directionResultsString = directionList.join('\n\n');
+    });
 }
 
 module.exports = {
