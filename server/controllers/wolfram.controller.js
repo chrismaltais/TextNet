67c8b0b77e64f53172a92b624b73b69d5b69e976
Chris Maltais <chris.maltais@hotmail.ca>
2019-03-20 11:50:14 -0400

    🎨 Tabs > Spaces

--- server/controllers/wolfram.controller.js
+++ server/controllers/wolfram.controller.js
@@ -1,28 +1,28 @@
 const axios = require("axios");
 
 async function getResponse(message) {
-  let response;
-  let query = message.toString();
+    let response;
+    let query = message.toString();
 
-  // Check if keyword "query" was used
-  if (query.split(" ")[0].toUpperCase() === "QUERY") {
-    let queryIndexBegin = query.toUpperCase().indexOf("QUERY") + 5;
-    query = query.substring(queryIndexBegin).trim();
-  }
+    // Check if keyword "query" was used
+    if (query.split(" ")[0].toUpperCase() === "QUERY") {
+        let queryIndexBegin = query.toUpperCase().indexOf("QUERY") + 5;
+        query = query.substring(queryIndexBegin).trim();
+    }
 
-  try {
-    response = await axios.get(
-      `http://api.wolframalpha.com/v1/result?appid=${process.env.WOLFRAM_APP_ID}&i=${query}`
-    );
-  } catch (e) {
-    errorString = `Invalid query: Unfortunately TextNet isn't smart enough to figure that one out. Ask us another question! :)`
-    return errorString
-  }
+    try {
+        response = await axios.get(
+      ` http://api.wolframalpha.com/v1/result?appid=${process.env.WOLFRAM_APP_ID}&i=${query}`
+      );
+    } catch (e) {
+        errorString = `Invalid query: Unfortunately TextNet isn't smart enough to figure that one out. Ask us another question! :)`
+        return errorString
+    }
 
-  let result = response.data.toString().replace("Wolfram Alpha", "TextNet");
-  return result;
+    let result = response.data.toString().replace("Wolfram Alpha", "TextNet");
+    return result;
 }
 
 module.exports = {
-  getResponse
+    getResponse
 };
