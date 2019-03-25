12b2b7e621ea7d7dc7e7cfb09f6e27459b69a342
Chris Maltais <chris.maltais@hotmail.ca>
2019-03-25 10:32:18 -0400

    ✅ Add initial unit tests for translate controller

--- server/controllers/translate.controller.js
+++ server/controllers/translate.controller.js
@@ -3,28 +3,20 @@ const fs = require("fs");
 const path = require('path');
 
 async function translate(stringToTranslate, languageTo) {
+    const { Translate } = require("@google-cloud/translate");
 
     // Instantiates a client
     const translateInstance = new Translate();
-    let languageCodesJSON = fs.readFileSync(path.resolve(__dirname, "../helpers/languageCode.json"));
-    let languages = JSON.parse(languageCodesJSON);
-    var langCode = null;
-    for (var i = 0; i < languages.length; i++) {
-        if (languageTo.toLowerCase() === languages[i]["name"].toLowerCase()) {
-            langCode = languages[i]["code"];
-        }
-    }
+
+    let languages = await getAllLanguages();
+    var langCode = await getLanguageCode(languages, languageTo);
 
     if (!langCode) {
         return `Unfortunately, ${languageTo} is not a language we can translate to!`
     }
 
-    let translatedResponses = await translateInstance.translate(stringToTranslate, langCode)
-    console.log(translatedResponses)
-
     try {
-        
-        
+        let translatedResponses = await translateInstance.translate(stringToTranslate, langCode)
         const translation = translatedResponses[0];
         console.log(`Text: ${stringToTranslate}`);
         console.log(`Translation: ${translation}`);
@@ -34,27 +26,65 @@ async function translate(stringToTranslate, languageTo) {
     }
 }
 
-async function getResponse(message) {
+async function getAllLanguages() {
+    let languageCodesJSON = fs.readFileSync(path.resolve(__dirname, "../helpers/languageCode.json"));
+    let languages = JSON.parse(languageCodesJSON);
+    return languages;
+}
 
-    if (message.lastIndexOf(" to ") == -1){
+async function getLanguageCode(languagesAvailable, targetLanguage) {
+    for (var i = 0; i < languagesAvailable.length; i++) {
+        if (targetLanguage.toLowerCase() === languagesAvailable[i]["name"].toLowerCase()) {
+            return languagesAvailable[i]["code"];
+        }
+    }
+    return null;
+}
+
+async function getResponse(message) {
+    let isValidQuery = await checkValidQuery(message);
+    if (!isValidQuery){
         return 'Invalid translation formatting.  You\'re missing the "to" keyword! Texting "textnet" will return our formatting guidelines :)';
     }
   
-    var stringToTranslate = message.substring(
+    let stringToTranslate = await getStringToTranslate(message);
+
+    let languageTo = await getTargetLanguage(message);
+
+    var result = await translate(stringToTranslate, languageTo);
+  
+    return result;
+}
+
+async function checkValidQuery(message) {
+    if (message.lastIndexOf(" to ") == -1){
+        return Promise.reject(false);
+    } 
+    return Promise.resolve(true);
+}
+
+async function getStringToTranslate(message) {
+    let stringToTranslate = message.substring(
         message.lastIndexOf("translate ") + 10,
         message.lastIndexOf("to ")
     ).trim();
+    return stringToTranslate;
+}
 
-    var languageTo = message.substring(
+async function getTargetLanguage(message) {
+    let targetLanguage = message.substring(
         message.lastIndexOf("to ") + 3, 
         message.lastIndexOf("")
     ).trim();
-
-  var result = await translate(stringToTranslate, languageTo);
-  
-  return result;
+    return targetLanguage;
 }
 
 module.exports = {
-  getResponse
+  getResponse,
+  checkValidQuery,
+  getStringToTranslate,
+  getTargetLanguage,
+  getLanguageCode,
+  getAllLanguages,
+  translate
 };
---
+++ server/tests/translate.controller.test.js
@@ -0,0 +1,85 @@
+const {translate} = require('./../controllers');
+//const {directionsAssert} = require('./assertion_data');
+
+describe('Translate Controller:', () => {
+    describe('Function: checkValidQuery(message)', () => {
+        it('should return true if query is valid', async () => {
+            let message = 'translate hello to french';
+            let result = translate.checkValidQuery(message);
+            expect(result).toBeTruthy();
+        });
+
+        it('should return false if missing keyword \'to\'', async () => {
+            let message = 'translate hello french';
+            let empty = {};
+            let result = translate.checkValidQuery(message);
+            expect(result).rejects.toBeFalsy();
+        });
+    });
+
+    describe('Function: getStringToTranslate(message)', () => {
+        it('should return desired string to translate from query', async () => {
+            let message = 'translate hello to french';
+            let result = await translate.getStringToTranslate(message);
+            expect(result).toBe('hello');
+        });
+    });
+
+    describe('Function: getTargetLanguage(message)', () => {
+        it('should return desired target language from query', async () => {
+            let message = 'translate hello to french';
+            let result = await translate.getTargetLanguage(message);
+            expect(result).toBe('french');
+        });
+    });
+
+    describe('Function: getAllLanguages()', () => {
+        it('should return array of all possible languages to translate to', async () => {
+            let result = await translate.getAllLanguages();
+            expect(result.length).toBe(104); // Number of languages
+        });
+    });
+
+    describe('Function: getLanguageCode(languagesAvailable, targetLanguage)', () => {
+        it('should return desired target language from query', async () => {
+            let targetLanguage = 'french';
+            let languagesAvailable = await translate.getAllLanguages();
+            let result = await translate.getLanguageCode(languagesAvailable, targetLanguage);
+            expect(result).toBe('fr');
+        });
+
+        it('should return null if target language cannot be translated to', async () => {
+            let targetLanguage = 'abcd';
+            let languagesAvailable = await translate.getAllLanguages();
+            let result = await translate.getLanguageCode(languagesAvailable, targetLanguage);
+            expect(result).toBeNull();
+        })
+    });
+
+    describe('Function: translate(stringToTranslate, languageTo)', () => {
+        it('should return a translated string', async () => {
+            // To do
+        });
+
+        it('should return an error if the service is not active', async () => {
+            let stringToTranslate = 'hello';
+            let languageTo = 'french';
+            let expectedResult = `Unfortunately we can\'t translate that right now, please try again later!`
+            let result = await translate.translate(stringToTranslate, languageTo);
+            expect(result).toBe(expectedResult);
+        });
+    });
+
+    describe('Function: getResponse(message', () => {
+        it('should parse an incoming query, and return a translated string', async () => {
+            // To do
+        });
+
+        it('should return an error if the service is unavailable', async () => {
+            let message = 'translate hello to french';
+            let expectedResult = `Unfortunately we can\'t translate that right now, please try again later!`
+            let result = await translate.getResponse(message);
+            expect(result).toBe(expectedResult);
+        });
+    });
+});
